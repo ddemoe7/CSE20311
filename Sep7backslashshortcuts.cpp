ab2d2e65ddc17e5c1c5c7a08556d2457eba1b9da
pkwak1 <pkwak@Pauls-MacBook-Pro.local>
2016-09-07 10:29:48 -0400

    backslash commands

---
+++ Sep7backslashshortcuts.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std
+/*
+Escape sequence
+newline
+\n
+horizontal tab
+\t
+vertical tab
+\v
+backspace
+\b
+carriage return
+\r
+form feed
+\f
+alert
+\a
+backslash
+\\
+question mark
+? or \?
+*/
