4888ba2676d5ec7acd380095bec6d3cdc4179807
pkwak1 <pkwak@pauls-mbp.dhcp.nd.edu>
2016-09-13 14:56:24 -0400

    lab3

--- lab3/gcdmain.cpp
+++ lab3/gcdmain.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 //prototyping function
-int gcd(int, int);
+int getgcd(int, int);
 
 int main() {
   //declaring and assigning variables
@@ -14,14 +14,14 @@ int main() {
   cin >> b;
 
   //gcd calculated
-  int ans = gcd(a,b);
-  cout << "The gcd of " << a << " and " << b << " is " << ans << "." << endl;
+  int gcd = getgcd(a,b);
+  cout << "The gcd of " << a << " and " << b << " is " << gcd << "." << endl;
 
 }
 
 //Using Euclid's Algorithm
 //Function for Greatest Common Divisor
-int gcd(int a, int b){
+int getgcd(int a, int b){
   int c;
   while (a != 0) {
     c = a; a = b%a; b=c;
