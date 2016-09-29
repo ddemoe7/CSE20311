8a8b4ae3b8a2f0113ed76fc086849fc739fe1474
pkwak1 <pkwak@Pauls-MacBook-Pro.local>
2016-09-29 18:46:59 -0400

    "test rev"

---
+++ Review1/test1.c
@@ -0,0 +1,21 @@
+//* NOT ACTUALLY A C FILE, DO NOT COMPILE *
+//1) Indentation
+#include <stdio.h>
+int main()
+{
+  int a, b, n = 4, s = 0;
+  for (a = 1 ; a < n ; a++)
+  {
+    for (b = 1; b < a; b++)
+    {
+      if (b == a/2)
+      {
+        s = s + a*b;
+        printf("%d ", s);
+      }
+      printf("\n");
+    }
+  }
+}
+//And then the program output wil be:
+//Indentation Notes:
---
+++ Review1/test2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int x = (7 / 2 * 2);
+  cout << x;
+}
